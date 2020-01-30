e620e5b12637d38e66f3b9926470ab7dc4172285
laitsky <vincent.diamond15@gmail.com>
2020-01-30 20:41:06 +0700

    token visual

--- src/app/oprec/oprec-token/oprec-token.component.ts
+++ src/app/oprec/oprec-token/oprec-token.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var particlesJS: any;
 @Component({
   selector: 'app-oprec-token',
   templateUrl: './oprec-token.component.html',
   styleUrls: ['./oprec-token.component.css']
 })
 export class OprecTokenComponent implements OnInit {
-
+  oprecToken: string;
   constructor() { }
 
   ngOnInit() {
+    particlesJS.load("particles-js", "assets/token_particles.json", null);
   }
 
 }
