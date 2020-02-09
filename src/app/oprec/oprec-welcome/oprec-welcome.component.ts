aed0bb87274b7f70967571c1a9517ecd41bd4719
laitsky <vincent.diamond15@gmail.com>
2020-02-09 13:42:27 +0700

    oprec-welcome.component.ts indentation

--- src/app/oprec/oprec-welcome/oprec-welcome.component.ts
+++ src/app/oprec/oprec-welcome/oprec-welcome.component.ts
@@ -14,7 +14,7 @@ export class OprecWelcomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    let vid = <HTMLVideoElement> document.getElementById("myVideo");
+    let vid = <HTMLVideoElement>document.getElementById("myVideo");
     vid.muted = true;
     this.tombol_mulai();
     $('#start-button').hover(function () {
@@ -62,23 +62,23 @@ export class OprecWelcomeComponent implements OnInit {
       <i class="las la-clock"></i>
       <p> Pendaftaran sudah ditutup! </p>`;
     } else {
-        let countDownDate = new Date("Feb 14, 2020 18:30:00").getTime();
-        let x = setInterval(function () {
-          let now = new Date().getTime();
-          let distance = countDownDate - now;
+      let countDownDate = new Date("Feb 14, 2020 18:30:00").getTime();
+      let x = setInterval(function () {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
 
-          // Time calculations for days, hours, minutes and seconds
-          let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-          // Display the result in the element with id="demo"
-          document.getElementById("status_regis").innerHTML = `
+        // Display the result in the element with id="demo"
+        document.getElementById("status_regis").innerHTML = `
           <i class="las la-clock"></i> <br>
           Pendaftaran akan ditutup dalam <br>
           ${days} hari ${hours} jam ${minutes} menit ${seconds} detik`;
-        }, 1000);
+      }, 1000);
     }
   }
 }
