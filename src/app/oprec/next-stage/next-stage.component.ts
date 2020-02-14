c343c00b2af4f967c668082cfeeaeb1b6c75b743
laitsky <vincent.diamond15@gmail.com>
2020-02-15 00:31:46 +0700

    next stage done

--- src/app/oprec/next-stage/next-stage.component.ts
+++ src/app/oprec/next-stage/next-stage.component.ts
@@ -11,6 +11,8 @@ export class NextStageComponent implements OnInit {
   public dataOprec: NextStage[];
   nimInput: number;
   isExist = false;
+  nimNotFound = false;
+  codeZero = false;
 
   constructor(private oprecApiService: OprecApiService) {
   }
@@ -23,25 +25,44 @@ export class NextStageComponent implements OnInit {
     this.isExist = false;
     this.oprecApiService.getNextStage().subscribe(result => {
         this.dataOprec = result;
+        console.log(this.dataOprec);
         // tslint:disable-next-line:prefer-for-of
         for (let i = 0; i < this.dataOprec.length; i++) {
           if (this.dataOprec[i].nim == this.nimInput) {
+            this.codeZero = false;
             this.isExist = true;
-            document.getElementById('pesan').innerHTML = "Pesan: " + this.dataOprec[i].pesan;
-            document.getElementById('nim').innerHTML = "NIM: " + this.dataOprec[i].nim;
-            document.getElementById('nama').innerHTML = "Nama: " + this.dataOprec[i].nama_lengkap;
-            document.getElementById('prodi').innerHTML = "Prodi: " + this.dataOprec[i].prodi;
-            document.getElementById('divisi').innerHTML = "Divisi: " + this.dataOprec[i].divisi;
-            document.getElementById('ruangan').innerHTML = "Ruangan: " + this.dataOprec[i].ruangan;
+            this.nimNotFound = false;
+            if (this.dataOprec[i].kode == 1) {
+              document.getElementById('pesan').innerHTML = "<h1>SELAMAT!</h1>";
+              document.getElementById('nama').innerHTML = this.dataOprec[i].nama_lengkap;
+              document.getElementById('nim').innerHTML = "<h4> 000000" + this.dataOprec[i].nim + "</h4>";
+              document.getElementById('divisi').innerHTML = "<h3>Divisi " + this.dataOprec[i].divisi + "</h3>";
+              document.getElementById('ruangan').innerHTML = "<h2>" + this.dataOprec[i].ruangan + "</h2>";
+              document.getElementById('daful-msg').innerHTML = "<h3> SEGERA LAKUKAN DAFTAR ULANG </h3><h6>klik tautan dibawah</h6>";
+              document.getElementById('daful-link').innerHTML = '<h4><a href="https://s.id/mxm-daful" target=_blank>s.id/mxm-daful</a></h4>';
+            } else if (this.dataOprec[i].kode == 0) {
+              document.getElementById('pesan').innerHTML = "";
+              document.getElementById('nama').innerHTML = "";
+              document.getElementById('nim').innerHTML = "";
+              document.getElementById('divisi').innerHTML = "";
+              document.getElementById('tanggal').innerHTML = '';
+              document.getElementById('ruangan').innerHTML = "";
+              document.getElementById('daful-msg').innerHTML = "";
+              document.getElementById('daful-link').innerHTML = '';
+              this.codeZero = true;
+            }
           }
         }
         if (this.isExist === false) {
-          document.getElementById('pesan').innerHTML = "Maaf, NIM tidak ditemukan!";
-          document.getElementById('nim').innerHTML = "";
+          this.nimNotFound = true;
+          document.getElementById('pesan').innerHTML = "";
           document.getElementById('nama').innerHTML = "";
-          document.getElementById('prodi').innerHTML = "";
+          document.getElementById('nim').innerHTML = "";
           document.getElementById('divisi').innerHTML = "";
+          document.getElementById('tanggal').innerHTML = '';
           document.getElementById('ruangan').innerHTML = "";
+          document.getElementById('daful-msg').innerHTML = "";
+          document.getElementById('daful-link').innerHTML = '';
         }
       }
     );
