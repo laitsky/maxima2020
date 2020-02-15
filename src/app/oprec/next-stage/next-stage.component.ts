37b894493beaf1fd5299505b4e568d99821c6f03
lumiere13 <60005583+lumiere13@users.noreply.github.com>
2020-02-15 14:16:54 +0700

    loading api

--- src/app/oprec/next-stage/next-stage.component.ts
+++ src/app/oprec/next-stage/next-stage.component.ts
@@ -28,48 +28,49 @@ export class NextStageComponent implements OnInit {
   cariData() {
     document.getElementById('loadingSign').style.display = 'inline';
     setTimeout(this.loadingSign, 1000);
-    this.isExist = false;
-    this.oprecApiService.getNextStage().subscribe(result => {
-        this.dataOprec = result;
-        // tslint:disable-next-line:prefer-for-of
-        for (let i = 0; i < this.dataOprec.length; i++) {
-          if (this.dataOprec[i].nim == this.nimInput) {
-            this.codeZero = false;
-            this.isExist = true;
-            this.nimNotFound = false;
-            if (this.dataOprec[i].kode == 1) {
-              document.getElementById('pesan').innerHTML = "<h1>SELAMAT!</h1>";
-              document.getElementById('nama').innerHTML = this.dataOprec[i].nama_lengkap;
-              document.getElementById('nim').innerHTML = "<h4> 000000" + this.dataOprec[i].nim + "</h4>";
-              document.getElementById('divisi').innerHTML = "<h3>Divisi " + this.dataOprec[i].divisi + "</h3>";
-              document.getElementById('ruangan').innerHTML = "<h2>" + this.dataOprec[i].ruangan + "</h2>";
-              document.getElementById('daful-msg').innerHTML = "<h3> SEGERA LAKUKAN DAFTAR ULANG </h3><h6>klik tautan dibawah</h6>";
-              document.getElementById('daful-link').innerHTML = '<h4><a href="https://s.id/mxm-daful" target=_blank>s.id/mxm-daful</a></h4>';
-            } else if (this.dataOprec[i].kode == 0) {
-              document.getElementById('pesan').innerHTML = "";
-              document.getElementById('nama').innerHTML = "";
-              document.getElementById('nim').innerHTML = "";
-              document.getElementById('divisi').innerHTML = "";
-              document.getElementById('tanggal').innerHTML = '';
-              document.getElementById('ruangan').innerHTML = "";
-              document.getElementById('daful-msg').innerHTML = "";
-              document.getElementById('daful-link').innerHTML = '';
-              this.codeZero = true;
+    setTimeout(() => { 
+      this.isExist = false;
+      this.oprecApiService.getNextStage().subscribe(result => {
+          this.dataOprec = result;
+          // tslint:disable-next-line:prefer-for-of
+          for (let i = 0; i < this.dataOprec.length; i++) {
+            if (this.dataOprec[i].nim == this.nimInput) {
+              this.codeZero = false;
+              this.isExist = true;
+              this.nimNotFound = false;
+              if (this.dataOprec[i].kode == 1) {
+                document.getElementById('pesan').innerHTML = "<h1>SELAMAT!</h1>";
+                document.getElementById('nama').innerHTML = this.dataOprec[i].nama_lengkap;
+                document.getElementById('nim').innerHTML = "<h4> 000000" + this.dataOprec[i].nim + "</h4>";
+                document.getElementById('divisi').innerHTML = "<h3>Divisi " + this.dataOprec[i].divisi + "</h3>";
+                document.getElementById('ruangan').innerHTML = "<h2>" + this.dataOprec[i].ruangan + "</h2>";
+                document.getElementById('daful-msg').innerHTML = "<h3> SEGERA LAKUKAN DAFTAR ULANG </h3><h6>klik tautan dibawah</h6>";
+                document.getElementById('daful-link').innerHTML = '<h4><a href="https://s.id/mxm-daful" target=_blank>s.id/mxm-daful</a></h4>';
+              } else if (this.dataOprec[i].kode == 0) {
+                document.getElementById('pesan').innerHTML = "";
+                document.getElementById('nama').innerHTML = "";
+                document.getElementById('nim').innerHTML = "";
+                document.getElementById('divisi').innerHTML = "";
+                document.getElementById('tanggal').innerHTML = '';
+                document.getElementById('ruangan').innerHTML = "";
+                document.getElementById('daful-msg').innerHTML = "";
+                document.getElementById('daful-link').innerHTML = '';
+                this.codeZero = true;
+              }
             }
           }
+          if (this.isExist === false) {
+            this.nimNotFound = true;
+            document.getElementById('pesan').innerHTML = "";
+            document.getElementById('nama').innerHTML = "";
+            document.getElementById('nim').innerHTML = "";
+            document.getElementById('divisi').innerHTML = "";
+            document.getElementById('tanggal').innerHTML = '';
+            document.getElementById('ruangan').innerHTML = "";
+            document.getElementById('daful-msg').innerHTML = "";
+            document.getElementById('daful-link').innerHTML = '';
+          }
         }
-        if (this.isExist === false) {
-          this.nimNotFound = true;
-          document.getElementById('pesan').innerHTML = "";
-          document.getElementById('nama').innerHTML = "";
-          document.getElementById('nim').innerHTML = "";
-          document.getElementById('divisi').innerHTML = "";
-          document.getElementById('tanggal').innerHTML = '';
-          document.getElementById('ruangan').innerHTML = "";
-          document.getElementById('daful-msg').innerHTML = "";
-          document.getElementById('daful-link').innerHTML = '';
-        }
-      }
-    );
+    );}, 1000);
   }
 }
